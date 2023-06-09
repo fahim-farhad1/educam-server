fb82b0062768149a6f009e9fe71fe2b4a01eec03
Fahim Farhad <fahimfarhad@Fahims-MacBook-Air.local>
2023-06-09 19:55:05 +0600

    install and setup express server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require('dotenv').config()
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello Tumi!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
