e5efcb91f8a0aa046fc1ee3cb6506d89ff190fc5
Fahim Farhad <fahimfarhad@Fahims-MacBook-Air.local>
2023-06-09 20:23:39 +0600

    query on populer field base on boolean data type

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //DB_Collections
+    const courseCollection = client.db('educamDB').collection('courses');
+    const reviewCollection = client.db('educamDB').collection('review');
+
+    app.get('/popular', async(req, res) =>{
+        const result = await courseCollection.find({ popular: true }).toArray();
+        res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -31,7 +42,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
